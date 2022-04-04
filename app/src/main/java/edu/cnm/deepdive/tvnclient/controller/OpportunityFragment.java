308c114fa427e0ac24fc019dbf35a52d1fe0570a
Cathy Bastareaud <catybastareaud@gmail.com>
2022-04-04 15:17:47 -0600

    Added navigation drawer menu and updated main activity

---
+++ app/src/main/java/edu/cnm/deepdive/tvnclient/controller/OpportunityFragment.java
@@ -0,0 +1,7 @@
+package edu.cnm.deepdive.tvnclient.controller;
+
+import androidx.fragment.app.Fragment;
+
+public class OpportunityFragment extends Fragment {
+
+}
---
+++ app/src/main/java/edu/cnm/deepdive/tvnclient/controller/ProfileFragment.java
@@ -0,0 +1,11 @@
+package edu.cnm.deepdive.tvnclient.controller;
+
+import androidx.fragment.app.Fragment;
+
+public class ProfileFragment extends Fragment {
+
+
+
+
+
+}
